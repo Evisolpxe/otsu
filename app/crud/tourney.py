967ffce159e1452f206b98997594c0c9959b1476
Evis <evisolpxe@126.com>
2020-05-29 10:19:01 +0800

    Update tourney.py

--- app/crud/tourney.py
+++ app/crud/tourney.py
@@ -1,13 +1,14 @@
 from mongoengine.errors import ValidationError
 from fastapi import HTTPException, status
 
-from app import models, schemas
+from app import schemas
+from app.models.tourney import Tourney
 
 
-def get_tourney(name: str) -> models.Tourney.objects:
-    full = models.Tourney.objects(tourney_name=name).first()
-    acronym = models.Tourney.objects(acronym=name).first()
-    chn = models.Tourney.objects(chn_name=name).first()
+def get_tourney(name: str) -> Tourney.objects:
+    full = Tourney.objects(tourney_name=name).first()
+    acronym = Tourney.objects(acronym=name).first()
+    chn = Tourney.objects(chn_name=name).first()
 
     if full or acronym or chn:
         return full or acronym or chn
@@ -15,7 +16,7 @@ def get_tourney(name: str) -> models.Tourney.objects:
 
 def create_tourney(t: schemas.tourney.Tourney) -> None:
     try:
-        tourney = models.Tourney(**dict(t))
+        tourney = Tourney(**dict(t))
         tourney.save()
     except ValidationError as e:
         raise HTTPException(status_code=status.HTTP_411_LENGTH_REQUIRED, detail=str(e))
