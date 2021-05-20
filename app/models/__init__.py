5464fcf100ff9e8ae0e8757aee6ca57b9c432210
Evisolpxe <evisolpxe@gmail.com>
2021-05-21 01:52:04 +0800

    修改默认库名

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,6 @@ from mongoengine import connect
 
 from global_config import MONGO_CONNECTION_STRING
 
-connect(host=MONGO_CONNECTION_STRING)
+connect(db='otsu-v3-test', host=MONGO_CONNECTION_STRING)
 
 
