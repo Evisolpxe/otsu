528cd637132a3d163d5474519aa8849221f1fc7c
Evis <evisolpxe@gmail.com>
2021-06-29 20:09:21 +0800

    完成光斗力基本计算方法，增加user_id翻译器

--- app/core/light_power.py
+++ app/core/light_power.py
@@ -1,6 +1,7 @@
 from typing import List
 from operator import attrgetter
 from itertools import groupby
+from collections import defaultdict
 
 from app.models.matches import Match, MatchScore
 
@@ -8,7 +9,30 @@ from app.models.matches import Match, MatchScore
 class LightPower:
 
     def __init__(self, matches: List[Match]):
-        self.grouped_game = groupby([game for match in matches for game in match.games], key=attrgetter('beatmap_id'))
+        games = sorted([game for match in matches for game in match.games], key=attrgetter('beatmap_id'))
 
-    def calc_avg_score(self, scores: List[MatchScore]):
-        pass
+        self.grouped_game = groupby(games, key=attrgetter('beatmap_id'))
+        self.light_power_item = defaultdict(list)
+
+        self.run()
+
+        self.light_power = {k: round(sum(v) / len(v), 4) for k, v in self.light_power_item.items()}
+
+    @staticmethod
+    def calc_avg_score(scores: List[MatchScore]) -> int:
+        if scores:
+            return int(sum([score.score for score in scores]) / len(scores))
+
+    def run(self):
+        for beatmap_id, games in self.grouped_game:
+            scores = [score for game in games for score in game.scores]
+            avg_score = self.calc_avg_score(scores)
+            for score in scores:
+                self.light_power_item[str(score.user_id)].append(round(score.score / avg_score, 5))
+
+
+# OCLR = [85172398, 84972689, 85044339, 84896923, 85501582, 85439390, 85503945, 85546490, 86302764, 86362224]
+#
+# C = LightPower([Match.get_match(i) for i in OCLR])
+#
+# print(C.light_power)
--- app/router/endpoints/users.py
+++ app/router/endpoints/users.py
@@ -19,3 +19,12 @@ async def get_user(user_id: int, elo_festival: str):
     if user := elo.UserElo.get_user_elo(user_id, elo_festival):
         return user
     raise HTTPException(404, detail={'message': '没有这位玩家的信息哦。'})
+
+
+@router.post('/user_id_translator',
+             summary='User_id翻译器。')
+async def user_id_translator(data: dict):
+    """把user_id为key的字典丢进来，自动翻译成username"""
+    if isinstance(data, dict):
+        return {users.User.get_user(k).username: v for k, v in data.items()}
+    raise HTTPException(406)
